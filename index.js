91a8cf86cdfe45689b07c36278989d5ee73dd733
ayhand <ayhandani@hotmail.com>
2021-04-13 01:39:16 +0200

    sooper fucking fixes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function register() {
-	let messaging = firebase.messaging();
-	messaging.getToken({
-		vapidKey: 'BJFCgqW_ott5wcl4J4EgKzaSP8nSp8E7pQVAHJgtpUJFfgGw3f7w_5fvSbhJeTvq3tXqQh9NdGKh9A98LNkpD7c'
-	});
-}
+$(() => {
+	function register() {
+		let messaging = firebase.messaging();
+		messaging.getToken({
+			vapidKey: 'BJFCgqW_ott5wcl4J4EgKzaSP8nSp8E7pQVAHJgtpUJFfgGw3f7w_5fvSbhJeTvq3tXqQh9NdGKh9A98LNkpD7c'
+		});
+	}
 
-$("#regbutton").on("click", () => {
-	register();
+	$("#regbutton").on("click", () => {
+		register();
+	});
 });
