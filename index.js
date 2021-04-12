bda99cec0f01e00d279479b0625c63e77eeca079
ayhand <ayhandani@hotmail.com>
2021-04-13 01:51:15 +0200

    service worker reg fix

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 $(() => {
-	function register() {
-		let messaging = firebase.messaging();
-		messaging.getToken({
-			vapidKey: 'BJFCgqW_ott5wcl4J4EgKzaSP8nSp8E7pQVAHJgtpUJFfgGw3f7w_5fvSbhJeTvq3tXqQh9NdGKh9A98LNkpD7c'
+	async function register() {
+		let reg = await navigator.serviceWorker.register(
+			"noti-sw.js", {
+			scope: 'firebase-cloud-messaging-push-scope'
 		});
+		let messaging = firebase.messaging();
+		let token = await messaging.getToken({
+			vapidKey: 'BJFCgqW_ott5wcl4J4EgKzaSP8nSp8E7pQVAHJgtpUJFfgGw3f7w_5fvSbhJeTvq3tXqQh9NdGKh9A98LNkpD7c',
+			serviceWorkerRegistration: reg
+		});
+		console.log(token);
 	}
 
 	$("#regbutton").on("click", () => {
